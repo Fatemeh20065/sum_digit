1c096ef16002ed83797e60e0b54fab93cd84aaab
fatemeh mardani <fatemehmardani138520065@gmail.com>
2026-01-02 12:06:28 +0330

    sum_digit

---
+++ sum_digit/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sum_digit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter number");
+            int num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("sum_digit : "+sumdigit(num));
+            Console.ReadKey();
+        }
+        static int sumdigit(int n)
+        {
+            if (n < 10)
+            {
+                return n;
+            }
+            else
+            {
+                return n % 10 + sumdigit(n / 10);
+            }
+        }
+    }
+}
